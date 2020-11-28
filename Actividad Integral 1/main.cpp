0babb5d427dd7e45604b34e3c9c93dc60cb68d12
A01706702 <65085262+A01706702@users.noreply.github.com>
2020-11-27 23:58:18 -0600

    Update main.cpp

--- Actividad Integral 1/main.cpp
+++ Actividad Integral 1/main.cpp
@@ -14,8 +14,12 @@ int main(){
     ifstream archivo;
     ofstream write;
 
+// se lee el archivo y se imprime en canciones.txt
     verArchivo(vinictor,"canciones.txt");
 
+    //menu de funciones
+    //es importante saber que el menú aún no esta listo, pues en ciertos casos el programa truena
+    //checar casos de prueba
 	cout << "===== Bienvenido =====" << endl;
     while (opcion != 4){
         cout << "-Ver lista(1)" << endl;
@@ -25,8 +29,10 @@ int main(){
         cout<< "-->";
         cin >> opcion;
 
+//en esta parte aun no se limitan los inputs para que no sean string, por lo que si se pasa un string se loopea
         if (opcion==1){
             vercancion(archivo);
+
         }
         else if (opcion==2){
 			sorts.mergeSort(vinictor);
@@ -43,7 +49,22 @@ int main(){
 			}
         }
         else if (opcion==3){
-            buscarCancion(archivo);
+            string cancionbuscada;
+            cout<<"Escribe la cancion que quieres buscar: ";
+            cin>>cancionbuscada;
+            buscarCancion(archivo, cancionbuscada);
+            // caso de prueba 1
+            // buscarCancion(archivo, "time")
+            // lo que devuelve es "La cancion es: time pinkfloyd 1973"
+
+            // caso de prueba 2
+            // buscarCancion(archivo, "pinkfloyd")
+            // Imprime "La cancion no existe" porque el parámetro de busqueda es por cancion y no por artista
+
+            // caso de prueba 3
+            // buscarCancion(archivo, "time pinkfloyd")
+            // se loopea porque no acepta espacios como input
         }
     }
+    return 0;
 }
